5d9cd56a46606bcaeadfd08d83ca5a94f9091bab
Nick Smith <nick.smith@vodafone.com>
2025-02-05 10:42:54 +0000

    Homepage login ctx

--- client/src/pages/homepage/Homepage.tsx
+++ client/src/pages/homepage/Homepage.tsx
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { UserContext } from "../../context/UserContext";
 import styles from "./styles.module.scss";
 
 const Homepage = () => {
+  const userContext = useContext(UserContext);
+
   return (
     <div className={styles.container}>
       <h1>Java Project Home</h1>
-      <p>by Nick Smith</p>
-      <div>
-        <Link to="/login">
-          <button>Login</button>
-        </Link>
-        <Link to="/create-account">
-          <button>Create Account</button>
-        </Link>
-      </div>
+      <p>By Nick Smith</p>
+      {userContext?.user ? (
+        <p>
+          Welcome {userContext.user.username}. Account type:{" "}
+          {userContext.user.role}.
+        </p>
+      ) : (
+        <div>
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
+          <Link to="/create-account">
+            <button>Create Account</button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
