b90ae0109236768404d3783eb5a6d91b1a5dc1bc
Nick Smith <nick.smith@vodafone.com>
2025-02-05 10:39:00 +0000

    Logout functionality

--- client/src/components/Navigation/Navigation.tsx
+++ client/src/components/Navigation/Navigation.tsx
@@ -1,15 +1,21 @@
 import React, { useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import styles from "./Navigation.module.scss";
 import { UserContext } from "../../context/UserContext";
 
 const Navigation = () => {
   const userContext = useContext(UserContext);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    userContext?.setUser(null);
+    navigate("/login");
+  };
 
   const links = userContext?.user
     ? [
         { name: "Home", url: "/" },
-        { name: "Logout", url: "/logout" },
+        { name: "Logout", url: "#", onClick: handleLogout },
       ]
     : [
         { name: "Home", url: "/" },
@@ -21,9 +27,15 @@ const Navigation = () => {
       <ul className={styles.navList}>
         {links.map((link) => (
           <li key={link.url} className={styles.navItem}>
-            <Link to={link.url} className={styles.navLink}>
-              {link.name}
-            </Link>
+            {link.onClick ? (
+              <span onClick={link.onClick} className={styles.navLink}>
+                {link.name}
+              </span>
+            ) : (
+              <Link to={link.url} className={styles.navLink}>
+                {link.name}
+              </Link>
+            )}
           </li>
         ))}
       </ul>
