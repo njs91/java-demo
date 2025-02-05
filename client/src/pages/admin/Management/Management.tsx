9afd4b17dd3d1ec9a32439c799645e088bfb86ff
Nick Smith <nick.smith@vodafone.com>
2025-02-05 15:58:50 +0000

    Notes and unused imports

--- client/src/pages/admin/Management/Management.tsx
+++ client/src/pages/admin/Management/Management.tsx
@@ -1,14 +1,26 @@
-import React, {
-  useContext,
-  useEffect,
-  useState,
-  ChangeEvent,
-  FormEvent,
-} from "react";
+import React, { useContext, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { UserContext } from "../../../context/UserContext";
 import CreateProduct from "./CreateProduct";
 import styles from "./styles.module.scss";
+
+/*
+    lists users and products
+    
+    functionality:
+    - CRUD a product:
+      - create nearly done - needs image upload
+      - need read
+      - need update
+      - need delete
+    - CRUD a user:
+      - need create
+      - need read
+      - need update
+      - need delete
+    - filter by order/cost/whatever
+    - search for a user
+  */
 
 const AdminManagement = () => {
   const { user } = useContext(UserContext) || {};
